3dc5f0896cf9e404a241c1d69810bc53eb521b0f
AdiSaptaguna <saptagunaadi@gmail.com>
2025-04-19 13:22:50 +0700

    Pertemuan4

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(){
+    var namaMhs = "Adi Saptaguna";
+    return(
+        <div>
+        <h3>ini Halaman footer</h3>
+        <h3>Design By Sapta</h3>
+        <p>Nama saya : {namaMhs} </p>
+        </div>
+    );
+}
+export default Footer;
